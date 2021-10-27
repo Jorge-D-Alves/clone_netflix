cee4dbffcf79597026271b837866365fe9624194
Jorge Alves <alvesdjorge@gmail.com>
2021-10-27 01:39:44 -0300

    ultimos ajustes

--- js/login.js
+++ js/login.js
@@ -1,15 +1,17 @@
 let email = document.getElementById('email')
 email.addEventListener('focus',()=>{
-    email.style.borderColor = "#4a5f6a";
+  email.style.borderColor = "#4a5f6a";
 });
+
 email.addEventListener('blur',()=>{
-    email.style.borderColor = "#ccc";
+  email.style.borderColor = "#ccc";
 });
 
 let password = document.getElementById('password');
 password.addEventListener('focus',()=>{
-    password.style.borderColor = "#4a5f6a";
+  password.style.borderColor = "#4a5f6a";
 });
+
 password.addEventListener('blur',()=>{
-    password.style.borderColor = "#ccc";
-});   
+  password.style.borderColor = "#ccc";
+});
